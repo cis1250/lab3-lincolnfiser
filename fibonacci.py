16bbfb6ca3fc9008a6634d3f006cfa69c725c018
lincolnfiser <lincfiser07@gmail.com>
2025-10-01 11:35:44 -0400

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,34 +1,19 @@
 # Fibonacci Sequence Exercise
 
-
 while True:
-    user_input = input("Enter the number of terms in the Fibonacci sequence: ")
-
-    # Validate input
-    if user_input.isdigit() and int(user_input) > 0:
-        terms = int(user_input)
-        break
-    else:
+    try:
+        terms = int(input("Enter the number of terms in the Fibonacci sequence: "))
+        if terms > 0:
+            break
+        else:
+            print("Please enter a positive integer.")
+    except ValueError:
         print("Please enter a positive integer.")
 
 # Calculate Fibonacci sequence
-fib_sequence = []
 a, b = 0, 1
 for _ in range(terms):
-    fib_sequence.append(a)
+    print(a, end=" ")
     a, b = b, a + b
 
-# Print all numbers in one line
-print(" ".join(map(str, fib_sequence)))
-
-
-
-
-
-
-# Fibonacci Sequence Exercise
-# TODO: (Read detailed instructions in the Readme file)
-# Prompt the user for the number of terms.
-# Validate that the input is a positive integer.
-# Use a for loop to print the Fibonacci sequence up to that many terms.
-
+print()  # newline at the end
