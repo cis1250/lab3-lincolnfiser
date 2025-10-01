4bb4e052a07e07da2f90b1625bf3daccd42e4932
lincolnfiser <lincfiser07@gmail.com>
2025-10-01 09:28:58 -0400

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -21,3 +21,11 @@ for _ in range(terms):
 print()  # new line at the end
 
 
+
+#!/usr/bin/env python3
+# Fibonacci Sequence Exercise
+# TODO: (Read detailed instructions in the Readme file)
+# Prompt the user for the number of terms.
+# Validate that the input is a positive integer.
+# Use a for loop to print the Fibonacci sequence up to that many terms.
+
